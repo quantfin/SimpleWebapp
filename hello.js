4a23bf75dd4e14c43ff47463574ecbfcd49c9775
Akash Ranjan <akash.hallo@gmail.com>
2021-10-02 13:25:52 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,24 @@
+// source code
+// js syntax
+
+var count=56;
+count=count +1;
+console.log("Hello World");
+console.log("first result=" +count);
+
+if(count <=100)
+{
+	console.log("Value is less than 100");
+}
+else
+{	console.log("value is greater than 100);
+}
+
+// defining function
+
+var show=function(){
+	console.log("Procesing show function");
+}
+
+//invoke function:
+show();
